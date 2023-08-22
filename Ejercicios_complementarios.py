8477db2572f259ab59bec84760d644af0069fd50
Santiago Chepilla <82857800+Santi759@users.noreply.github.com>
2023-08-22 18:28:30 -0300

    Add files via upload

---
+++ Ejercicios_complementarios.py
@@ -0,0 +1,71 @@
+"""Ejercicios Complementarios"""
+
+#Ejercicio_1
+numero_1 = 5
+
+#Ejercicio_2
+numero_2 = 2.5
+
+#Ejercicio_3
+suma = numero_1 + numero_2
+
+#Ejercicio_4
+resta = numero_1 - numero_2
+multiplicacion = numero_1 * numero_2
+division = numero_1/numero_2
+print("La suma es igual a =" ,suma)
+print("La resta es igual a =" ,resta)
+print("La multiplicacion es igual a =" ,multiplicacion)
+print("La division es igual a =" ,division)
+
+#Ejercicio_5
+nombre = "Santiago"
+
+#Ejercicio_6
+precio = 500.50
+
+#Ejercicio_7
+descuento = 0.50
+
+#Ejercicio_8
+precio_final = precio*descuento
+print("El precio final es = " ,precio_final)
+
+#Ejercicio_9
+cadena = "Quinteros"
+
+#Ejercicio_10
+print(len(cadena))
+
+#Ejercicio_11
+print(int(precio))
+
+#Ejercicio_12
+nombre= "Santiago"
+apellido = " Chepilla"
+nombre_completo = nombre + apellido
+print (nombre_completo)
+
+#Ejercicio_13
+edad = 19 
+print("Su edad es: " ,edad)
+edad= 19+5
+print("Su edad se incremento a:" ,edad)
+edad= edad-10
+print("Su edad se disminuyo a: " ,edad)
+
+#Ejercicio_14
+altura = 1.85
+print("Tu altura es: " ,altura)
+altura=altura*4
+print("Tu altura es: " ,altura)
+altura=altura/3
+print("Tu altura es: " ,altura)
+
+#Ejercicio_15
+nombre_2 = "SANTIAGO".lower()
+print(nombre_2)
+
+#Ejercicio_16
+nombre_2 = "SANTIAGO".capitalize()
+print(nombre_2)
